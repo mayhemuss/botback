d9ad3cfa5b8670367fb8f6e5db41a6a6fe32f981
mayhemus <mayhemus@yandex.ru>
2024-08-17 21:46:23 +0300

    text fix

--- games/gamesList.js
+++ games/gamesList.js
@@ -6,7 +6,7 @@ export const gamesList = [
     dateStart: +(new Date(2024, 7, 19, 10, 30)),
     dateEnd: +(new Date(2024, 7, 30, 23, 59, 59)),
     descriptions: "Регистрация на новый турнир по Dota 2, Выбери как зарегистрироваться, " +
-      "как капитан или как член комманды?",
+      "как капитан команды или как член команды?",
     registrationSheets: "Dotareg31_08_24",
     imageUrl: "https://sun9-50.userapi.com/impg/XsxpMN9gEg2vxS1I0gTtOwVd7kAPqd-lnvzN1g/4Lu7rjvPjB0.jpg?size=1920x1080&quality=95&sign=311e0ef60315b00c9269fe1dcdb24cfb&type=album",
     reglaments: texts.capitanReg,
