201118f156c82414614d69a5feadb3d37a566912
mayhemus <mayhemus@yandex.ru>
2024-09-02 11:45:09 +0300

    valorant start

--- games/gamesList.js
+++ games/gamesList.js
@@ -2,19 +2,34 @@ import {texts} from "../text.js";
 
 export const gamesList = [
   {
-    gameName: "Dota 2",
-    dateStart: +(new Date(2024, 7, 19, 16 - 3, 30)),
-    dateEnd: +(new Date(2024, 7, 30, 23 - 3, 59)),
-    descriptions: "Регистрация на новый турнир по Dota 2, Выбери как зарегистрироваться, " +
+    gameName: "Valorant",
+    dateStart: +(new Date(2024, 8, 5, 15 - 3,)),
+    dateEnd: +(new Date(2024, 8, 14, 13 - 3,)),
+    descriptions: `Регистрация на новый турнир по Valorant, Выбери как зарегистрироваться, ` +
       "как капитан команды или как член команды?",
-    registrationSheets: "Dotareg31_08_24",
-    imageUrl: "https://sun9-50.userapi.com/impg/XsxpMN9gEg2vxS1I0gTtOwVd7kAPqd-lnvzN1g/4Lu7rjvPjB0.jpg?size=1920x1080&quality=95&sign=311e0ef60315b00c9269fe1dcdb24cfb&type=album",
-    reglaments: "С Регламентом турнира по Dota 2 можно ознакомится по данной ссылке: https://clck.ru/3CgxHQ \n \n" + texts.capitanReg +
+    registrationSheets: "Valorantreg15_09_24",
+    imageUrl: "https://sun9-10.userapi.com/impg/VRViQ9AoEKkQUt4osvUKZejGRnlgJv-swUL62g/wZotEI5Pmw8.jpg?size=1920x1080&quality=95&sign=c20d66cb74f314a67147e82eeef96581&type=album",
+    reglaments: `С Регламентом турнира по Valorant можно ознакомится по данной ссылке: https://clck.ru/3CvSqM \n \n` + texts.capitanReg +
       "\n \nДля подтверждения участия мы позвоним вашему капитану за 2-3 дня до кибер-турнира. Удачи на SkyNet GAMES!",
     commandMemberCount: 5,
-    webAppUrl: "https://skynetgamesbot.ru",
-    anonced: "dota",
-    anoncedPost: "https://t.me/games_skynet/448"
+    webAppUrl: "https://skynetgamesbot.ru/app",
+    anonced: "valorant",
+    anoncedPost: "https://t.me/games_skynet/",
+    type: "game"
   },
-
+  // {
+  //   gameName: "Iphone",
+  //   dateStart: +(new Date(2024, 8, 15, 15 - 3,)),
+  //   dateEnd: +(new Date(2024, 8, 30, 13 - 3,)),
+  //   descriptions: 'Пригласи трех друзей, которые небыли подписаны на SkyNet Games и получи шанс выйграть айфон',
+  //   registrationSheets: "IphoneReg30_09_24",
+  //   newMemberSheets: "IphoneNewMemberReg30_09_24",
+  //   imageUrl: "https://3dnews.ru/assets/external/illustrations/2023/09/19/1093280/iPhone15_02.jpg",
+  //   reglaments: `Гога тут щас напишет невьебаться какой текст с регламентом конкурса на айфоны`,
+  //   commandMemberCount: 3,
+  //   webAppUrl: "https://skynetgamesbot.ru/iphone",
+  //   anonced: "iphone",
+  //   anoncedPost: "https://t.me/games_skynet/",
+  //   type: "lottery"
+  // },
 ]
